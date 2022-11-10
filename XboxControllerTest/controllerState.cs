f572cb517f06fa35525b90f6927a4d9d2d4a6bc5
squiblez <squiblez@gmail.com>
2022-11-10 17:37:42 -0500

    Finalize controllerState class and add full parser for Xbox gamepad specific bytes

--- XboxControllerTest/controllerState.cs
+++ XboxControllerTest/controllerState.cs
@@ -23,9 +23,37 @@
 using System;
 namespace XboxControllerTest
 {
+    /// <summary>
+    /// Abstraction used by the linuxXboxController to check the indivdual state of each button and axis between updates.
+    /// </summary>
     public class controllerState
     {
         public bool[] buttons = new bool[0x0B];
+        public short[] axis = new short[0x09];
+
+        //Get the state of a button via enumerator.
+        public bool getButtonState(xButtons button)
+        {
+            bool output = false;
+            try
+            {
+                output = buttons[(byte)button];
+            }
+            catch { }
+            return output;
+        }
+
+        //Get the state of an axis via enumerator.
+        public short getAxisState(xEvents _axis)
+        {
+            short output = 0;
+            try
+            {
+                output = axis[(byte)_axis];
+            }
+            catch { }
+            return output;
+        }
 
         public controllerState()
         {
